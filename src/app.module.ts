dc70693d40a263dbc709021e959118ae71c31a61
majorlynch <majorlynch@gmail.com>
2025-12-12 09:48:37 +0000

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { KeysModule } from './keys/keys.module';
+import { ChatModule } from './chat/chat.module';
+import { ImageModule } from './image/image.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes ConfigService available app-wide
+      envFilePath: '.env', // default is .env; adjust if needed
+    }),
+    KeysModule,
+    ChatModule,
+    ImageModule,
+  ],
+})
+export class AppModule {}
